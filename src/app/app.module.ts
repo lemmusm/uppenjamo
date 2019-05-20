bd753c6d04109454909aeac4e9fd63c1b0be4943
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-05-20 14:05:59 -0500

    actualizaciones en contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 // import mdbootstrapmodule
 import { MdbootstrapModule } from './mdbootstrap.module';
+// ng-bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +28,8 @@ import { CarrerasComponent } from './components/shared/carreras/carreras.compone
     CommonModule,
     NgtUniversalModule,
     AppRoutingModule,
-    MdbootstrapModule
+    MdbootstrapModule,
+    NgbModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
   providers: []
